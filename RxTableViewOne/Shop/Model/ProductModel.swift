36135999a5ddc945ffc60f7c5a52b39dcf1b63c0
Lenx <lenvzyl@gmail.com>
2021-07-07 09:14:46 +0200

    RxTableView

---
+++ RxTableViewOne/Shop/Model/ProductModel.swift
@@ -0,0 +1,7 @@
+
+
+import Foundation
+
+struct Product {
+    let imageName, name, price: String
+}
---
+++ RxTableViewOne/Shop/View/Cell/ProductTableViewCell.swift
@@ -0,0 +1,30 @@
+
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var holderView: UIView!
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        holderView.layer.cornerRadius = 10
+        productImg.layer.cornerRadius = productImg.frame.width / 2
+        productImg.clipsToBounds = true
+    }
+    
+    var item: Product! {
+        didSet {
+            setProductData()
+        }
+    }
+
+    private func setProductData() {
+        productImg.image = UIImage(named: item.imageName)
+        nameLbl.text = item.name
+        priceLbl.text = item.price
+    }
+}
